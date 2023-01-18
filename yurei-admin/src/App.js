ac553792bbaa529a74599603d055795ef9e5634e
Tuan Anh Do <dotuananh15031999@gmail.com>
2023-01-18 07:09:52 +0700

    api memberlist

--- yurei-admin/src/App.js
+++ yurei-admin/src/App.js
@@ -1,24 +1,29 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-import { getAnimePosts, getComicPosts } from './apis';
+import { getAnimePosts, getComicPosts, getMemberList } from './apis';
 import Box from '@mui/material/Box';
+import { Typography } from '@mui/material';
 
 function App() {
   const [postsAnime, setPostsAnime] = useState()
   const [postsComic, setPostsComic] = useState()
+  const [memberList, setMemberList] = useState()
 
   useEffect(() => {
     getAnimePosts().then(response => setPostsAnime(response))
     getComicPosts().then(response => setPostsComic(response))
+    getMemberList().then(response => setMemberList(response))
+    console.log(memberList)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
   return (
     <Box>
       <Box sx={{mb: '40px'}}>
-        <h1 style={{color: 'red'}}>Animes: {postsAnime && postsAnime.length}</h1>
+        <Typography sx={{color: 'red'}}>Animes: {postsAnime && postsAnime.length}</Typography>
         {postsAnime ? postsAnime.items.map(item => <h1 key={item.id}>{item.title}</h1>) : <>Loading...</>} 
       </Box>   
       <Box>
-        <h1 style={{color: 'red'}}>Comics: {postsComic && postsComic.length}</h1>
+        <Typography sx={{color: 'red'}}>Comics: {postsComic && postsComic.length}</Typography>
         {postsComic ? postsComic.items.map(item => <h1 key={item.id}>{item.title}</h1>) : <>Loading...</>} 
       </Box>   
     </Box>
--- yurei-admin/src/apis/index.js
+++ yurei-admin/src/apis/index.js
@@ -1,4 +1,5 @@
 import API from "../utils/http";
+import axios from "axios";
 
 const Global = {
     ANIME_BLOG_ID: "7311086410835747408",
@@ -34,4 +35,13 @@ export const getComicPosts = async () => {
     } catch (error) {
       return error;
     }
+}
+
+export const getMemberList = async () => {
+  try {
+    const response = await axios.get(`https://opensheet.elk.sh/1PrUFQj43Aqpq7UP6InyO-Ez0aHtgpFVVvo9946UXsXY/1`);
+    console.log(response)
+  } catch (error) {
+    return error;
+  }
 }
